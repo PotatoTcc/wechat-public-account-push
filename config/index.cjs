6e61b61730a9f879bb5e0e9bab6d3d7df34b5f04
PotatoTcc <111333282+PotatoTcc@users.noreply.github.com>
2023-04-19 21:45:43 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -114,7 +114,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oBoCq6FKqnqZzoh-veT9N0UMxl4c',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '9QBY_HMp2pvFnN37Az0FoPOc_x8wf-bnih-Leovl06w',
+      useTemplateId: 'ChlEFGdzWLy-vZYc2IlOBF8ukKuJxYVD0mxSJHLvrS0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -203,7 +203,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'QinZ0cD9MflS02sw8bWQbfJcsvpsbf2d770SRnsDyf4',
+  CALLBACK_TEMPLATE_ID: 'ChlEFGdzWLy-vZYc2IlOBF8ukKuJxYVD0mxSJHLvrS0',
 
   CALLBACK_USERS: [
     {
